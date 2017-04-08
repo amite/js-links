653f901b382cb47fdd67d5268f23899675016f02
amite <amit.erandole@gmail.com>
2017-04-08 22:45:17 +0530

    added missing proptypes

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-const Header = (props) => (
+const Header = ({title}) => (
   <header>
-    <h1>{ props.title }</h1>
+    <h1>{ title }</h1>
   </header>
 );
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+}
 
 export default Header;
